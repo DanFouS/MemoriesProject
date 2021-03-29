c0aad8459d56b4f48b0e27fde66178434b323327
DanFouS <rouzmfawer@gmail.com>
2021-03-29 22:46:12 +0200

    post section

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,8 @@
+import postMessage from "../models/postMessage.js";
+
 export const getPosts = (req, res) => {
   res.send("Posts Works!");
+};
+export const createPost = (req, res) => {
+  res.send("Creating Post");
 };
