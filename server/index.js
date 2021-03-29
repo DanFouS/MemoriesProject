327920af9c27b571685621043d47fb71892f59cc
DanFouS <rouzmfawer@gmail.com>
2021-03-29 22:08:25 +0200

    server side

--- server/index.js
+++ server/index.js
@@ -9,3 +9,20 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://memory:memory123123@cluster0.j609g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 4070;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`server running on port:${PORT}`))
+  )
+  .catch((error) => console.error(error.message));
+
+mongoose.set("useFindAndModify", false);
