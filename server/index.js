84f0693e2656fbb1e583ced455b043147bfdc67f
DanFouS <rouzmfawer@gmail.com>
2021-03-29 21:41:28 +0200

    yarn

---
+++ server/index.js
@@ -0,0 +1,11 @@
+import express from "experss";
+import mongoose from "mongoose";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+app.use(cors());
