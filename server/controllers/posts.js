5aa91d6518de3bf5917ae1803330b7a30121d351
DanFouS <rouzmfawer@gmail.com>
2021-03-29 22:49:41 +0200

    getPosts

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,7 +1,13 @@
 import postMessage from "../models/postMessage.js";
 
-export const getPosts = (req, res) => {
-  res.send("Posts Works!");
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await postMessage.find();
+    console.log(postMessages);
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error });
+  }
 };
 export const createPost = (req, res) => {
   res.send("Creating Post");
