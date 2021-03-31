8a8f9a57505e3f9ba472c7eaf1a2a7cecbbfbdae
DanFouS <rouzmfawer@gmail.com>
2021-03-31 10:12:24 +0200

    JSX Structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+
+import Posts from "./components/Posts/Posts.js";
+import Form from "./components/Form/Form.js";
+
 import memories from "./images/memories.png";
 const App = () => {
   return (
@@ -10,8 +14,22 @@ const App = () => {
         </Typography>
         <img src={memories} alt="memories" />
       </AppBar>
-      <Grow>
-        
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
       </Grow>
     </Container>
   );
